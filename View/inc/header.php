f3ab2a31983be7b01705bc68483fa7431192f688
Colinleg <80259244+colinleg@users.noreply.github.com>
2021-08-02 22:46:09 +0200

    clean
    - nettoyage du static
    - tailwind CDN
    - logo

--- View/inc/header.php
+++ View/inc/header.php
@@ -6,6 +6,8 @@
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="<?=ROOT_URL?>static/css/materialize.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="<?=ROOT_URL?>static/css/style.css"/>
+      <!-- Import Tailwind  -->
+      <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta charset="utf-8" />
